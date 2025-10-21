22ad8815bd5eee7756c7c05e0024e39a74c13732
iMex-1 <moukeddar236med@gmail.com>
2025-10-21 12:12:23 +0100

    Updated the home ui
    home ui updated and separated css styles

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import encg from '../assets/encg.jpeg';
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import encg from "../assets/encg.jpeg";
+import "./styles/navbar.css";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,7 +9,7 @@ const Navbar = () => {
   const [isTdOpen, setIsTdOpen] = useState(false);
   const location = useLocation();
 
-  const handleToggle = () => setIsOpen(prev => !prev);
+  const handleToggle = () => setIsOpen((prev) => !prev);
   const handleNavigate = () => {
     setIsOpen(false);
     setIsCoursOpen(false);
@@ -23,11 +24,6 @@ const Navbar = () => {
 
   return (
     <>
-      {/* ✅ Banner Section */}
-      <div className="banner-container">
-        <img src={encg} alt="ENCG Banner" className="banner-image" />
-      </div>
-
       {/* ✅ Single Navbar */}
       <nav className="navbar">
         <div className="navbar-container">
@@ -37,7 +33,7 @@ const Navbar = () => {
           </div>
 
           <button
-            className={`navbar-toggle${isOpen ? ' is-open' : ''}`}
+            className={`navbar-toggle${isOpen ? " is-open" : ""}`}
             aria-label="Toggle navigation menu"
             aria-expanded={isOpen}
             aria-controls="primary-navigation"
@@ -50,51 +46,91 @@ const Navbar = () => {
 
           <ul
             id="primary-navigation"
-            className={`navbar-menu${isOpen ? ' is-open' : ''}`}
+            className={`navbar-menu${isOpen ? " is-open" : ""}`}
           >
             <li className="navbar-item">
-              <Link to="/" className="navbar-link" onClick={handleNavigate}>Accueil</Link>
+              <Link to="/" className="navbar-link" onClick={handleNavigate}>
+                Accueil
+              </Link>
             </li>
 
-            <li className={`navbar-item dropdown${isCoursOpen ? ' is-open' : ''}`}>
+            <li
+              className={`navbar-item dropdown${isCoursOpen ? " is-open" : ""}`}
+            >
               <button
                 type="button"
                 className="navbar-link dropdown-toggle"
                 aria-haspopup="true"
                 aria-expanded={isCoursOpen}
-                onClick={() => setIsCoursOpen(prev => !prev)}
+                onClick={() => setIsCoursOpen((prev) => !prev)}
               >
                 Cours ▾
               </button>
               <ul className="dropdown-menu" role="menu">
-                <li><Link to="/cours/3eme" onClick={handleNavigate}>3éme année ENCG</Link></li>
-                <li><Link to="/cours/4eme" onClick={handleNavigate}>4éme année ENCG</Link></li>
-                <li><Link to="/cours/5eme" onClick={handleNavigate}>5éme année ENCG</Link></li>
+                <li>
+                  <Link to="/cours/3eme" onClick={handleNavigate}>
+                    3éme année ENCG
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/cours/4eme" onClick={handleNavigate}>
+                    4éme année ENCG
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/cours/5eme" onClick={handleNavigate}>
+                    5éme année ENCG
+                  </Link>
+                </li>
               </ul>
             </li>
 
-            <li className={`navbar-item dropdown${isTdOpen ? ' is-open' : ''}`}>
+            <li className={`navbar-item dropdown${isTdOpen ? " is-open" : ""}`}>
               <button
                 type="button"
                 className="navbar-link dropdown-toggle"
                 aria-haspopup="true"
                 aria-expanded={isTdOpen}
-                onClick={() => setIsTdOpen(prev => !prev)}
+                onClick={() => setIsTdOpen((prev) => !prev)}
               >
                 TD ▾
               </button>
               <ul className="dropdown-menu" role="menu">
-                <li><Link to="/td/3eme" onClick={handleNavigate}>3éme année ENCG</Link></li>
-                <li><Link to="/td/4eme" onClick={handleNavigate}>4éme année ENCG</Link></li>
-                <li><Link to="/td/5eme" onClick={handleNavigate}>5éme année ENCG</Link></li>
+                <li>
+                  <Link to="/td/3eme" onClick={handleNavigate}>
+                    3éme année ENCG
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/td/4eme" onClick={handleNavigate}>
+                    4éme année ENCG
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/td/5eme" onClick={handleNavigate}>
+                    5éme année ENCG
+                  </Link>
+                </li>
               </ul>
             </li>
 
             <li className="navbar-item">
-              <Link to="/a-propos" className="navbar-link" onClick={handleNavigate}>À propos</Link>
+              <Link
+                to="/a-propos"
+                className="navbar-link"
+                onClick={handleNavigate}
+              >
+                À propos
+              </Link>
             </li>
             <li className="navbar-item">
-              <Link to="/contact" className="navbar-link" onClick={handleNavigate}>Contact</Link>
+              <Link
+                to="/contact"
+                className="navbar-link"
+                onClick={handleNavigate}
+              >
+                Contact
+              </Link>
             </li>
           </ul>
         </div>
