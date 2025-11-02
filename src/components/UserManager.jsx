f1c5e0639a93c8fbe7f5886034eb5921b0f0ddd2
iMex-1 <moukeddar236med@gmail.com>
2025-11-02 22:11:40 +0100

    fixed som inputs

--- src/components/UserManager.jsx
+++ src/components/UserManager.jsx
@@ -420,7 +420,7 @@ const UserManager = () => {
                             <input
                                 type={showPassword ? "text" : "password"}
                                 name="password"
-                                placeholder={editingUser.uid ? 'Nouveau Mot de Passe (laisser vide pour ne pas changer)' : 'Mot de Passe (minimum 6 caractères)'}
+                                placeholder={editingUser.uid ? 'Nouveau Mot de Passe' : 'Mot de Passe'}
                                 value={passwordInput}
                                 onChange={handleInputChange}
                                 minLength="6"
